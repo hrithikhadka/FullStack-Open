d8a2f5989503746af0b7813e47d1fec79d6474fd
Hrithik Khadka <hritik.khadka@centria.fi>
2024-02-01 15:51:00 +0200

    2.10: The Phonebook Step 5 - refactor

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,61 @@
 import { useState } from "react";
 
+//Filter
+const Filter = ({ handleFilter }) => {
+  return (
+    <div>
+      filter shown wtih <input onChange={handleFilter} />
+    </div>
+  );
+};
+
+//PersonForm
+const PersonForm = ({
+  newName,
+  newNumber,
+  handleNewName,
+  handleNewNumber,
+  addNewPerson,
+}) => {
+  return (
+    <form onSubmit={addNewPerson}>
+      <h2>add a new</h2>
+      <div>
+        name: <input value={newName} onChange={handleNewName} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNewNumber} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+//Single Person
+const Person = ({ person }) => {
+  return (
+    <li>
+      {person.name}
+      {person.number}
+    </li>
+  );
+};
+
+//persons component
+const Persons = ({ persons, filterByName }) => {
+  return (
+    <ul>
+      {filterByName.length > 0
+        ? filterByName.map((person) => (
+            <Person key={person.name} person={person} />
+          ))
+        : persons.map((person) => <Person key={person.name} person={person} />)}
+    </ul>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
@@ -44,37 +100,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input onChange={handleFilter} />
-      </div>
 
-      <form onSubmit={addNewPerson}>
-        <h2>add a new</h2>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter handleFilter={handleFilter} />
+
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNewName={handleNewName}
+        handleNewNumber={handleNewNumber}
+        addNewPerson={addNewPerson}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {filterByName.length > 0
-          ? filterByName.map((person) => (
-              <li key={person.name}>
-                {person.name} {person.number}
-              </li>
-            ))
-          : persons.map((person) => (
-              <li key={person.name}>
-                {person.name}
-                {person.number}
-              </li>
-            ))}
-      </ul>
+
+      <Persons persons={persons} filterByName={filterByName} />
     </div>
   );
 };
