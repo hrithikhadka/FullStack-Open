aaa4e0b8318af2ead6663c463cb8f1dd80cd7476
Hrithik Khadka <hritik.khadka@centria.fi>
2024-02-09 08:35:22 +0200

    2.11: The Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 //Filter
 const Filter = ({ handleFilter }) => {
@@ -57,12 +58,7 @@ const Persons = ({ persons, filterByName }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterByName, setFilterByName] = useState([]);
@@ -96,6 +92,13 @@ const App = () => {
     );
     setFilterByName(filtered);
   };
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      // console.log(response.data);
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
