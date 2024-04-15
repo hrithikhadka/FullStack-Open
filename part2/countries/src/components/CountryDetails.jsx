4b23330a8fb7468cfdb331282dbfc637deeca415
Hrithik Khadka <hritik.khadka@centria.fi>
2024-04-15 21:57:37 +0300

    2.20*: Data for countries, step3

--- part2/countries/src/components/CountryDetails.jsx
+++ part2/countries/src/components/CountryDetails.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${API_KEY}
+&units=metric`
+      )
+      .then((response) => {
+        const data = response.data;
+        setWeather(data);
+        console.log(data);
+      });
+  }, [country.capital]);
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -11,6 +31,17 @@ const CountryDetails = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png} alt="Country Flag" />
+      {weather && (
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <p>temperature {weather.main.temp} Celsius</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+            alt="icon"
+          />
+          <p>wind {weather.wind.speed} m/s</p>
+        </div>
+      )}
     </div>
   );
 };
