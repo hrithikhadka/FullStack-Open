dd4f74d7e396cabd0f05c9a5f43009fcec247d65
hrithikhadka <hrithikhadka00@gmail.com>
2024-12-03 23:48:26 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -83,6 +83,37 @@ describe("total likes", () => {
   });
 });
 
+describe("favorite blog", () => {
+  test("of empty list is null", () => {
+    const result = listHelper.favoriteBlog(emptyList);
+    assert.strictEqual(result, null);
+  });
+
+  test("when list has only one blog", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(result, {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    });
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    });
+  });
+});
+
 test("dummy returns one", () => {
   const blogs = [];
 
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  return blogs.reduce((accFav, currentBlog) =>
+    currentBlog.likes > accFav.likes ? currentBlog : accFav
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
