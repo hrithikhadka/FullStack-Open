5e83783b4a77b8d7853ab66bbac01dfec66cf07f
hrithikhadka <hrithikhadka00@gmail.com>
2025-02-22 08:17:43 +0200

    4.8: Blog List Tests, step 1

---
+++ part4/bloglist/tests/bloglist_api_test.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blog");
+const assert = require("node:assert");
+const { test, after, beforeEach } = require("node:test");
+const api = supertest(app);
+
+const initialBlogs = [
+  {
+    title: "blog 1",
+    author: "harry",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 3,
+  },
+  {
+    title: "blog 2",
+    author: "pete",
+    url: "https://reactpatterns.com/",
+    likes: 9,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
+
+test.only("blogs are returned as JSON", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test.only("blog list application returns the correct amount of blog posts", async () => {
+  const response = await api.get("/api/blogs");
+  console.log("Total blogs returned:", response.body.length);
+  assert.strictEqual(response.body.length, initialBlogs.length);
+});
+
+after(() => {
+  mongoose.connection.close();
+});
